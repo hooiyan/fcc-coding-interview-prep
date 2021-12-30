4c7a8d70dd0337ebf03f1d9ba4f005c6f1db2ff8
hoeho <hoecoding@gmail.com>
2021-12-30 18:16:16 +0800

    Still struggling with 01-algorithms/02-find-the-symmetric-difference

--- 01-algorithms/01-find-the-symmetric-difference/index.js
+++ 01-algorithms/01-find-the-symmetric-difference/index.js
@@ -1,52 +1,36 @@
 function sym(...args) {
   let outerArray = args
   let outerArrayLength = args.length
-  let outerArray1Index = 0
-  let outerArray2Index = 1
-  let innerArray1Index = 0;
-  let innerArray2Index = 0;
+
+  let outerArrayStartingIndex = 0
+
+  let innerArray1NextIndex = 0;
+  let innerArray2NextIndex = 0;
+
   let finalResult = []
   
-  // Testing code
-  console.log(outerArrayLength)
-
-  while (innerArray2Index < outerArray[outerArray2Index].length) {
-    if (outerArray[outerArray1Index][innerArray1Index] === outerArray[outerArray2Index][innerArray2Index]) {
-      console.log(`${outerArray[outerArray1Index][innerArray1Index]} is equal to ${outerArray[outerArray2Index][innerArray2Index]}`)
-      // outerArray2Index++
-    } else {
-      finalResult.push(outerArray[outerArray1Index][innerArray1Index])
-      // outerArray2Index++
-    }
-    innerArray2Index++
-  }
-
-  console.log(`innerArray2Index: ${innerArray2Index}`)
-
-  while (true) {
-    if (outerArray[outerArray1Index][innerArray1Index] === outerArray[outerArray2Index][innerArray2Index]) {
-      console.log(`${outerArray[outerArray1Index][innerArray1Index]} is equal to ${outerArray[outerArray2Index][innerArray2Index]}`)
-      // outerArray2Index++
-    } else {
-      finalResult.push(outerArray[outerArray1Index][innerArray1Index])
-      // outerArray2Index++
-    }
-    innerArray2Index++
-  }
-
-  // do {
-  //   if (outerArray[outerArray1Index][innerArray1Index] === outerArray[outerArray2Index][innerArray2Index]) {
-  //     console.log(`${outerArray[outerArray1Index][innerArray1Index]} is equal to ${outerArray[outerArray2Index][innerArray2Index]}`)
-  //     // outerArray2Index++
-  //   } else {
-  //     finalResult.push(outerArray[outerArray1Index][innerArray1Index])
-  //     // outerArray2Index++
+  // while (outerArrayStartingIndex < outerArrayLength) {
+  //   while (outerArray[outerArrayStartingIndex + 1][innerArray2NextIndex] < outerArray[outerArrayStartingIndex + 1].length) {
+  //     if (outerArray[outerArrayStartingIndex][innerArray1NextIndex] === outerArray[outerArrayStartingIndex + 1][innerArray2NextIndex]) {
+  //       innerArray2NextIndex++
+  //       console.log(innerArray2NextIndex)
+  //     } else {
+  //       finalResult.push(outerArray[outerArrayStartingIndex][innerArray1NextIndex])
+  //       innerArray2NextIndex++
+  //       console.log(innerArray2NextIndex)
+  //     }
   //   }
-  //   innerArray2Index++
   // }
-  // while (innerArray2Index < outerArray[outerArray2Index].length)
 
-  console.log(`innerArray2Index: ${innerArray2Index}`)
+  if (outerArray[outerArrayStartingIndex][innerArray1NextIndex] === outerArray[outerArrayStartingIndex + 1][innerArray2NextIndex]) {
+    innerArray2NextIndex++
+    console.log(innerArray2NextIndex)
+  } else {
+    finalResult.push(outerArray[outerArrayStartingIndex][innerArray1NextIndex])
+    innerArray2NextIndex++
+    console.log(innerArray2NextIndex)
+  }
+
   console.log(`finalResult: ${finalResult}`)
 }
 
