8e08fa86da013d59541a726248efa89941cde77e
hoeho <hoecoding@gmail.com>
2021-12-30 16:43:15 +0800

    Try while & do...while loop

--- 01-algorithms/01-find-the-symmetric-difference/index.js
+++ 01-algorithms/01-find-the-symmetric-difference/index.js
@@ -1,30 +1,53 @@
 function sym(...args) {
-  const outerArray = args
-  const outerArrayLength = args.length
+  let outerArray = args
+  let outerArrayLength = args.length
   let outerArray1Index = 0
   let outerArray2Index = 1
+  let innerArray1Index = 0;
+  let innerArray2Index = 0;
   let finalResult = []
   
-  for(let i = 0; i < outerArrayLength; i++) {
-    
-    const innerArray = outerArray[i]
-    console.log(`array ${i + 1}`)
-    
-    for (let j = 0; j < innerArray.length; j++) {
+  // Testing code
+  console.log(outerArrayLength)
 
-      // 1. Check if the first index in array1 matches with
-      //    all indices in array2
-      if (outerArray[outerArray1Index][j] === outerArray[outerArray2Index][j]) {
-        console.log('true');
-      } else {
-        console.log('false');
-        finalResult.push(outerArray[outerArray1Index][j])
-      }
+  while (innerArray2Index < outerArray[outerArray2Index].length) {
+    if (outerArray[outerArray1Index][innerArray1Index] === outerArray[outerArray2Index][innerArray2Index]) {
+      console.log(`${outerArray[outerArray1Index][innerArray1Index]} is equal to ${outerArray[outerArray2Index][innerArray2Index]}`)
+      // outerArray2Index++
+    } else {
+      finalResult.push(outerArray[outerArray1Index][innerArray1Index])
+      // outerArray2Index++
     }
-
-    console.log("---")
-    console.log(finalResult)
+    innerArray2Index++
   }
+
+  console.log(`innerArray2Index: ${innerArray2Index}`)
+
+  while (true) {
+    if (outerArray[outerArray1Index][innerArray1Index] === outerArray[outerArray2Index][innerArray2Index]) {
+      console.log(`${outerArray[outerArray1Index][innerArray1Index]} is equal to ${outerArray[outerArray2Index][innerArray2Index]}`)
+      // outerArray2Index++
+    } else {
+      finalResult.push(outerArray[outerArray1Index][innerArray1Index])
+      // outerArray2Index++
+    }
+    innerArray2Index++
+  }
+
+  // do {
+  //   if (outerArray[outerArray1Index][innerArray1Index] === outerArray[outerArray2Index][innerArray2Index]) {
+  //     console.log(`${outerArray[outerArray1Index][innerArray1Index]} is equal to ${outerArray[outerArray2Index][innerArray2Index]}`)
+  //     // outerArray2Index++
+  //   } else {
+  //     finalResult.push(outerArray[outerArray1Index][innerArray1Index])
+  //     // outerArray2Index++
+  //   }
+  //   innerArray2Index++
+  // }
+  // while (innerArray2Index < outerArray[outerArray2Index].length)
+
+  console.log(`innerArray2Index: ${innerArray2Index}`)
+  console.log(`finalResult: ${finalResult}`)
 }
 
 sym([1, 2, 3], [5, 2, 1, 4], [4, 2, 6], [1, 9, 7]);
