6256052fc337c6577ee4cf164c1fe3bab8fb3cc8
hoeho <hoecoding@gmail.com>
2021-12-30 15:59:01 +0800

    Trying for loop

--- 01-algorithms/01-find-the-symmetric-difference/index.js
+++ 01-algorithms/01-find-the-symmetric-difference/index.js
@@ -1,32 +1,30 @@
 function sym(...args) {
-  const argsLength = args.length
   const outerArray = args
-  let outerArray1Index = 0;
-  let outerArray2Index = 1;
-  let outerArrayCount = argsLength - 1
-
-  for(let i = 0; i < argsLength; i++) {
-
-    console.log(`array ${i}`)
-    for (let j = 0; j < outerArray[i].length; j++) {
-      // const innerArr = outerArray[i][j]
-      // console.log(innerArray)
+  const outerArrayLength = args.length
+  let outerArray1Index = 0
+  let outerArray2Index = 1
+  let finalResult = []
+  
+  for(let i = 0; i < outerArrayLength; i++) {
+    
+    const innerArray = outerArray[i]
+    console.log(`array ${i + 1}`)
+    
+    for (let j = 0; j < innerArray.length; j++) {
 
       // 1. Check if the first index in array1 matches with
       //    all indices in array2
-      // if (outerArray[array1Index][j] === outerArray[array2Index][j]) {
-
-      // } 
-
-      console.log(outerArray[outerArray1Index][j])
-      console.log(outerArray[outerArray2Index][j])
+      if (outerArray[outerArray1Index][j] === outerArray[outerArray2Index][j]) {
+        console.log('true');
+      } else {
+        console.log('false');
+        finalResult.push(outerArray[outerArray1Index][j])
+      }
     }
 
     console.log("---")
+    console.log(finalResult)
   }
-
-  // console.log(argsLength, count)
-  return args
 }
 
 sym([1, 2, 3], [5, 2, 1, 4], [4, 2, 6], [1, 9, 7]);
