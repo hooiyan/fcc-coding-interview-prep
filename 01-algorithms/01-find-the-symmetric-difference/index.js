d0d5a677f530904b80c7d5fe20810c3e19110916
hoeho <hoecoding@gmail.com>
2021-12-30 15:12:01 +0800

    First commit

---
+++ 01-algorithms/01-find-the-symmetric-difference/index.js
@@ -0,0 +1,31 @@
+function sym(...args) {
+  const argsLength = args.length
+  const outerArray = args
+  let outerArray1Index = 0;
+  let outerArray2Index = 1;
+  let outerArrayCount = argsLength - 1
+
+  for(let i = 0; i < argsLength; i++) {
+
+    for (let j = 0; j < outerArray[i].length; j++) {
+      // const innerArr = outerArray[i][j]
+      // console.log(innerArray)
+
+      // 1. Check if the first index in array1 matches with
+      //    all indices in array2
+      // if (outerArray[array1Index][j] === outerArray[array2Index][j]) {
+
+      // } 
+
+      console.log(outerArray[array1Index][j])
+      console.log(outerArray[array2Index][j])
+    }
+
+    console.log("---")
+  }
+
+  // console.log(argsLength, count)
+  return args
+}
+
+sym([1, 2, 3], [5, 2, 1, 4], [4, 2, 6], [1, 9, 7]);
---
+++ README.md
@@ -0,0 +1,30 @@
+# freeCodeCamp Coding Interview Prep
+
+[Link](https://www.freecodecamp.org/learn/coding-interview-prep/)
+
+## Algorithms
+
+- [ ] Find the Symmetric Difference
+- [ ] Inventory Update
+- [ ] No Repeats Please
+- [ ] Pairwise
+- [ ] Implement Bubble Sort
+- [ ] Implement Selection Sort
+- [ ] Implement Insertion Sort
+- [ ] Implement Quick Sort
+- [ ] Implement Merge Sort
+- [ ] Implement Binary Search
+
+## Data Structures
+
+
+
+## Take Home Projects
+
+
+
+## Rosetta Code
+
+
+
+## Project Euler
